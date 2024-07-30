7a6fa9a827a22503d454539258bda39c463795f5
DeepeshKalura <deepeshkalurs@gmail.com>
2024-07-30 09:09:26 +0530

    intial starting point

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# term frequency 
+
+import os
+import sys
+
+# indexing loginc  hai 
+path = os.getcwd() + "/app/files"
+for filename in os.listdir(path):
+    with open(path+"/"+filename, "r") as f:
+        # Logic for indexing
+        pass
+
+
+
+
+
